b95ec1974478ad70af2f6ce289c080e31f9fb76e
chrstphrriazo <80978203+chrstphrriazo@users.noreply.github.com>
2022-06-02 16:55:16 +0800

    Add files via upload

---
+++ src/app/apixu.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApixuService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(location){
+    return this.http.get(
+        'http://api.weatherstack.com/current?access_key=88fa4bef0a1362c115e85c17a366175e&query=' + location
+    );
+
+  }
+
+}
